bcb9477cb888de008e4cc77014a204af38cee0c8
ponny343701-png <ponny3437.01@gmail.com>
2025-11-29 21:22:26 +0300

    Add function to calculate grade based on input value

---
+++ s.js
@@ -0,0 +1,15 @@
+function f(){
+let x=+a.value,y;
+if(x>92)y=4;
+else if(x>89)y=3.7;
+else if(x>86)y=3.3;
+else if(x>82)y=3;
+else if(x>79)y=2.7;
+else if(x>76)y=2.3;
+else if(x>72)y=2;
+else if(x>69)y=1.7;
+else if(x>66)y=1.3;
+else if(x>64)y=1;
+else y=0;
+b.innerHTML=y;
+}
